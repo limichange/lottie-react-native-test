6a3bf744b85cf3537920ced5c2f1f02a7f2432e7
limichange <limichange@hotmail.com>
2025-04-15 09:45:43 +0800

    Enhance App.tsx with button to toggle WebView visibility and update WebView source URL

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@
  * @format
  */
 import { WebView } from 'react-native-webview';
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   useColorScheme,
@@ -16,6 +17,7 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() !== 'dark';
+  const [showWebView, setShowWebView] = useState(false);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -26,9 +28,16 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
+          <Button
+            title={showWebView ? 'Close WebView' : 'Open WebView'}
+            onPress={() => {
+              setShowWebView(!showWebView);
+            }}
+          />
           <WebView
-            source={{ uri: 'https://tradingview.onekeytest.com/?k=19' }}
-            style={{ height: 800, width: '100%' }}
+            originWhitelist={['']}
+            source={{ uri: 'http://192.168.0.107:5173/' }}
+            style={{ height: 700, width: '100%' }}
           />
       </ScrollView>
     </SafeAreaView>
